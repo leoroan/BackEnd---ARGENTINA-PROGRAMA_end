0897b24877ff05e70d1ebbf02aab636ee60dc1da
Leandro M <leoroan@gmail.com>
2022-09-03 14:49:01 -0300

    springboot+Dependencies ADEDD

---
+++ src/main/java/com/miPortfolio/miPortfolioII/MiPortfolioIiApplication.java
@@ -0,0 +1,13 @@
+package com.miPortfolio.miPortfolioII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPortfolioIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPortfolioIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miPortfolio/miPortfolioII/MiPortfolioIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miPortfolio.miPortfolioII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPortfolioIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

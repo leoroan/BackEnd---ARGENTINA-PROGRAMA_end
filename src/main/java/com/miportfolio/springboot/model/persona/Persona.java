f1fe3b04f19ae8868003d9d57d3fc8a6ce469118
Leandro M <leoroan@gmail.com>
2023-01-05 18:17:59 -0300

    modifyed persona class, added red social links

--- src/main/java/com/miportfolio/springboot/model/persona/Persona.java
+++ src/main/java/com/miportfolio/springboot/model/persona/Persona.java
@@ -21,12 +21,15 @@ public class Persona {
   private String titulo_actual;
   private String sobre_mi;
   private String contrasenia;
+  private String linkedin;
+  private String facebook;
+  private String github;
 
   public Persona() {
   }
 
   public Persona(int id, String nombre, String apellido, String email, String fecha_nac, String titulo_actual,
-      String sobre_mi, String contrasenia) {
+      String sobre_mi, String contrasenia, String linkedin, String facebook, String github) {
     this.id = id;
     this.nombre = nombre;
     this.apellido = apellido;
@@ -35,7 +38,13 @@ public class Persona {
     this.titulo_actual = titulo_actual;
     this.sobre_mi = sobre_mi;
     this.contrasenia = contrasenia;
+    this.linkedin = linkedin;
+    this.facebook = facebook;
+    this.github = github;
   }
+  
+
+  
 
 
   
